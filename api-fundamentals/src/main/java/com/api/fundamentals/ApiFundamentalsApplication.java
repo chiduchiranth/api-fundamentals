a54b5444cec45a16485ecfb4427921836743b3f8
chiduchiranth <chiduchiranth@gmail.com>
2024-01-08 17:35:34 +0530

    Initial set up for API fundamentals

---
+++ api-fundamentals/src/main/java/com/api/fundamentals/ApiFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.api.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFundamentalsApplication.class, args);
+	}
+
+}
