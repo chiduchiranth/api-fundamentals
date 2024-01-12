8332113ec9412e9f4dcd7780af265438dadd4841
chiduchiranth <chiduchiranth@gmail.com>
2024-01-12 12:54:45 +0530

    Add files via upload

---
+++ api-fundamentals/src/main/java/com/api/fundamentals/model/Student.java
@@ -0,0 +1,61 @@
+package com.api.fundamentals.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "age")
+	private int age;
+
+	@Column(name = "branch")
+	private String branch;
+	
+	public Student() {}
+	
+
+	public Student(String name, int age, String branch) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.branch = branch;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+}
